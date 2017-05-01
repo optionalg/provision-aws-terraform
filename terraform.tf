0a88910a0ed76386596071d9a44e96999ccfedac
Tom Trahan <tom@shippable.com>
2017-05-01 12:06:09 -0700

    update availability zone

--- terraform.tf
+++ terraform.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "sampleInstances" {
 
   # ami = "${var.ecsAmi}"
   ami = "ami-80861296"
-  availability_zone = "us-east-1b"
+  availability_zone = "us-east-1a"
   instance_type = "t2.micro"
   subnet_id = "subnet-c0732fa5"
   associate_public_ip_address = true
